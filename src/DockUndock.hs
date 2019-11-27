6ac5fdf63f6c67b0ca0829d59e2b049ec2a5e90d
Markus Klinik <mkl@lambdanaut.net>
2019-11-27 10:54:31 +0100

    remove keyboard mouse

--- src/DockUndock.hs
+++ src/DockUndock.hs
@@ -35,18 +35,6 @@ pointerSetup = do
   disableAccelerationForAllPointers
 
 keyboardMisc = do
-  -- keyboard mouse aka mousekeys
-  -- NUM 1, 2, 3, 4, 6, 7, 8, 9   Direction control
-  -- NUM 5  Click
-  -- –  Switch to right click mode. Press 5 now to right click.
-  --
-  -- /  Switch to left click mode
-  -- 0 (INS)  Switch to select/drag mode. Now use direction control keys to select/drag stuff
-  -- . (DEL)  End select/drag
-  callProcess "xkbset" ["m"]
-  callProcess "xkbset" ["ma", "60", "10", "10", "5", "2"]
-  callProcess "xkbset" ["exp", "=m"]
-
   -- keyboard repeat rate
   callProcess "xset" ["r", "rate", "200", "50"]
 
