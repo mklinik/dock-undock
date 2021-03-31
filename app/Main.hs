8324c2b04c6f24fb6ae40ed5083a85de9e433e1d
Markus Klinik <mkl@lambdanaut.net>
2021-03-31 07:37:39 +0200

    add logging

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import System.Environment (getArgs)
@@ -6,20 +7,28 @@ import qualified Options
 import           Options (Mode(..))
 import DockUndock
 import Xrandr
+import System.Log.FastLogger
+import System.Log.FastLogger.Date
+
+logg :: ToLogStr a => TimedFastLogger -> a -> IO ()
+logg logger msg = logger (\time -> toLogStr time <> " " <> (toLogStr msg) <> "\n")
 
 main :: IO ()
 main = do
-  args <- getArgs
-  opts <- Options.get args
-  case Options.mode opts of
-    Help -> Options.printHelp
-    Dock -> xrandr >>= dock
-    Undock -> xrandr >>= undock
-    Autodock -> do
-      out <- xrandr
-      if isDocked out
-        then undock out
-        else dock out
-    Mouse -> do
-      pointerSetup
-      keyboardSetup
+  t <- newTimeCache "%Y-%d-%m %T"
+  withTimedFastLogger t (LogFileNoRotate "/home/mkl/dock.log" defaultBufSize) $ \f -> do
+    args <- getArgs
+    logg f $ show args
+    opts <- Options.get args
+    case Options.mode opts of
+      Help -> Options.printHelp
+      Dock -> xrandr >>= dock
+      Undock -> xrandr >>= undock
+      Autodock -> do
+        out <- xrandr
+        if isDocked out
+          then undock out
+          else dock out
+      Mouse -> do
+        pointerSetup
+        keyboardSetup
