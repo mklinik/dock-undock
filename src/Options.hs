d75c7a755f28b48dbdea8be691f2777791647a0a
Markus Klinik <mkl@lambdanaut.net>
2022-04-23 10:24:20 +0200

    make Dock the default action

--- src/Options.hs
+++ src/Options.hs
@@ -12,12 +12,12 @@ data Options = Options
 
 defaultOptions :: Options
 defaultOptions  = Options {
-    mode          = Help
+    mode          = Dock
   }
 
 options :: [OptDescr (Options -> Options)]
 options = [
-    Option [] ["help"] (NoArg  (\opts -> opts { mode = Help })) "display this help and exit"
+    Option ['h'] ["help"] (NoArg  (\opts -> opts { mode = Help })) "display this help and exit"
   , Option [] ["dock"] (NoArg  (\opts -> opts { mode = Dock })) "dock laptop"
   , Option [] ["undock"] (NoArg  (\opts -> opts { mode = Undock })) "undock laptop"
   , Option [] ["toggle"] (NoArg  (\opts -> opts { mode = Autodock })) "automatically determine if it should be dock or undock"
